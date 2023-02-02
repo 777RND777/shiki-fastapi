44236963551aa013cd6e502b8cc1e019502608fd
Nurmukhanbet Rakhimbayev <rakhimbayev.n@source.tnt>
2023-02-03 01:27:51 +0600

    text choice bug was fixed

--- shiki_fastapi/models.py
+++ shiki_fastapi/models.py
@@ -1,24 +1,58 @@
-import enum
-
-from sqlalchemy import Column, Enum, Float, ForeignKey, Integer, String, Table, Text
+from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table, Text, types
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
-
 association_table = Table(
-    "association_table",
+    'association_table',
     Base.metadata,
-    Column("anime_pk", ForeignKey("animes.pk")),
-    Column("genre_pk", ForeignKey("genres.pk")),
+    Column('anime_pk', ForeignKey('animes.pk')),
+    Column('genre_pk', ForeignKey('genres.pk')),
 )
+
+
+class ChoiceType(types.TypeDecorator):
+    impl = types.String
+
+    def __init__(self, choices, **kwargs):
+        self.choices = dict(choices)
+        super(ChoiceType, self).__init__(**kwargs)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.items() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
+
+
+ANIME_KIND_CHOICE = {
+    'TV_SERIES': 'tv',
+    'MOVIE': 'movie',
+    'OVA': 'ova',
+    'ONA': 'ona',
+    'SPECIAL': 'special',
+    'MUSIC': 'music'
+}
+ANIME_STATUS_CHOICE = {
+    'ANNOUNCED': 'anons',
+    'AIRING': 'ongoing',
+    'FINISHED': 'released'
+}
+REVIEW_STATUS_CHOICE = {
+    'PLANNED_TO_WATCH': 'planned',
+    'WATCHING': 'watching',
+    'REWATCHING': 'rewatching',
+    'COMPLETED': 'completed',
+    'ON_HOLD': 'on_hold',
+    'DROPPED': 'dropped'
+}
 
 
 class Genre(Base):
     __tablename__ = 'genres'
 
     pk = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    name = Column(String, unique=True, nullable=False)
 
     animes = relationship('Anime', secondary=association_table,
                           back_populates='genres', lazy=True)
@@ -31,7 +65,7 @@ class Studio(Base):
     __tablename__ = 'studios'
 
     pk = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
+    name = Column(String, unique=True, nullable=False)
 
     animes = relationship('Anime', back_populates='studio', lazy=True)
 
@@ -42,30 +76,17 @@ class Studio(Base):
 class Anime(Base):
     __tablename__ = 'animes'
 
-    class Kind(enum.Enum):
-        TV_SERIES = 'tv'
-        MOVIE = 'movie'
-        OVA = 'ova'
-        ONA = 'ona'
-        SPECIAL = 'special'
-        MUSIC = 'music'
-
-    class Status(enum.Enum):
-        ANNOUNCED = 'anons'
-        AIRING = 'ongoing'
-        FINISHED = 'released'
-
     pk = Column(Integer, primary_key=True)
-    title = Column(String, unique=True)
-    kind = Column(Enum(Kind))
-    episodes = Column(Integer)
-    status = Column(Enum(Status))
+    title = Column(String, unique=True, nullable=False)
+    kind = Column(ChoiceType(ANIME_KIND_CHOICE), nullable=False)
+    episodes = Column(Integer, nullable=False)
+    status = Column(ChoiceType(ANIME_STATUS_CHOICE), nullable=False)
     genres = relationship('Genre', secondary=association_table,
                           back_populates='animes', lazy=True)
     score = Column(Float)
-    studio_id = Column(Integer, ForeignKey('studios.pk'))
+    studio_id = Column(Integer, ForeignKey('studios.pk'), nullable=False)
     studio = relationship('Studio', back_populates='animes', lazy=True)
-    synopsis = Column(Text)
+    synopsis = Column(Text, nullable=False)
     reviews = relationship('Review', back_populates='anime', lazy=True)
 
     def __str__(self):
@@ -75,20 +96,12 @@ class Anime(Base):
 class Review(Base):
     __tablename__ = 'reviews'
 
-    class Status(enum.Enum):
-        PLANNED_TO_WATCH = 'planned'
-        WATCHING = 'watching'
-        REWATCHING = 'rewatching'
-        COMPLETED = 'completed'
-        ON_HOLD = 'on_hold'
-        DROPPED = 'dropped'
-
     pk = Column(Integer, primary_key=True)
-    anime_id = Column(Integer, ForeignKey('animes.pk'))
+    anime_id = Column(Integer, ForeignKey('animes.pk'), nullable=False)
     anime = relationship('Anime', back_populates='reviews', lazy=True)
-    user_id = Column(Integer, ForeignKey('users.pk'))
+    user_id = Column(Integer, ForeignKey('users.pk'), nullable=False)
     user = relationship('User', back_populates='reviews', lazy=True)
-    status = Column(Enum(Status))
+    status = Column(ChoiceType(REVIEW_STATUS_CHOICE), nullable=False)
     watched_episodes = Column(Integer)
     score = Column(Integer)
     text = Column(Text)
@@ -101,8 +114,8 @@ class User(Base):
     __tablename__ = 'users'
 
     pk = Column(Integer, primary_key=True)
-    username = Column(String, unique=True)
-    password = Column(String)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
     reviews = relationship('Review', back_populates='user', lazy=True)
 
     def __str__(self):
